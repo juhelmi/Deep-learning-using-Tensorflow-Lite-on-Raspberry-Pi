915c2e9768edcf18e1c34b8a4ddc2ca889d116c4
Juha Helminen <juha.helminen@iki.fi>
2025-07-26 16:29:30 +0300

    Routine names and directories tuned

--- Project_3: Audio Led Control/scripts/1_audio_recorder.py
+++ Project_3: Audio Led Control/scripts/1_audio_recorder.py
@@ -6,13 +6,17 @@ import time
 import os
 
 # Notebook expects 50 samples per category
+first_index_for_sample = 0
 number_of_repeats = 50
 categories = ['on', 'off', 'red', 'green']
+#number_of_repeats = 2
+#categories = ['green']
+#categories = ['nope']
 
 
 root_directory = os.path.dirname(os.path.abspath(__file__))
-root_directory = os.path.dirname(root_directory)
-data_directory = root_directory + '/data_jh/'
+root_directory = os.path.dirname(root_directory) + '/'
+data_directory = root_directory + 'data_jh/'
 
 if not os.path.exists(data_directory):
     print(f"Creating data directory: {data_directory}")
@@ -20,8 +24,11 @@ if not os.path.exists(data_directory):
 
 for category in categories:
     print(f"Current category: {category}")
+    if not os.path.exists(data_directory + category):
+        print(f"Creating directory for category: {category}")
+        os.makedirs(data_directory + category)
     time.sleep(1)  # Wait for a second before starting the recording
-    for i in range(number_of_repeats):
+    for i in range(first_index_for_sample, first_index_for_sample+number_of_repeats):
         #category = 'green'
         file_name = category+"_" + f"{i:02d}"  # Format the number with leading zeros
         duration = 1  # seconds
